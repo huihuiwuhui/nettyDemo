88dea718d1079b991dc3d9a997b02c0aef5e63cd
huihuiwuhui <zfeino1@163.com>
2018-03-19 15:43:15 +0800

    初始化项目

---
+++ src/main/java/com/itcv/netty/server/NettyServer.java
@@ -0,0 +1,11 @@
+package com.itcv.netty.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NettyServer {
+	private static Logger logger = LoggerFactory.getLogger(NettyServer.class);
+     public static void main(String[] args) {
+		logger.debug("开始启动服务！");
+	}
+}
